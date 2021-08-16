82571b1426b6aa2f264697a8e0a236f19e14bf60
N3r0m5ns3r <ziadh6@hotmail.com>
2021-08-16 06:23:40 +0200

    inital structure

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const uuidv1 = require('uuidv1');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: string,
+    trim: true,
+    required: true,
+    maxLength: 32
+  },
+  email: {
+    type: sting,
+    trim: true,
+    required: true,
+    unique: 32
+  },
+  hashed_password: {
+    type: string,
+    required: true
+  },
+  about: {
+    type: string,
+    trim: true
+  },
+  salt: string,
+  role: {
+    type:number,
+    default: 0
+  },
+  history: {
+    type: array,
+    default: []
+  }
+}, {timestamps: true});
+
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('hello, world! yo');
-});
+const {sayHi} = require('../controllers/user');
+
+router.get("/", sayHi);
 
 module.exports = router;
